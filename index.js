4325697e274219dcabdb83a6007b66b1d66110c8
lenizza1331 <leniyuva@gmail.com>
2021-11-15 03:02:12 +0300

    1

---
+++ index.js
@@ -0,0 +1,44 @@
+const inputField = document.querySelector('.inputField');
+const btn = document.querySelector('.btn');
+const toDoList = document.querySelector('.toDoList');
+
+
+btn.addEventListener('click', enter);
+
+inputField.addEventListener("keypress", enter);
+
+function enter(e) {
+    if (e.keyCode === 13 && inputField.value.length > 0) {
+        const item = document.createElement('li');
+    item.innerText = inputField.value;
+    item.classList.add('added');
+    toDoList.appendChild(item);
+    inputField.value = '';
+    item.addEventListener('click', ()=>{
+        item.classList.add('done');
+    });
+    item.addEventListener('dblclick', ()=>{
+        toDoList.removeChild(item);
+    })
+    }
+}
+
+
+
+
+// () => {
+//     if (inputField.value.length >0){
+//     const item = document.createElement('li');
+//     item.innerText = inputField.value;
+//     item.classList.add('added');
+//     toDoList.appendChild(item);
+//     inputField.value = '';
+
+//     item.addEventListener('click', ()=>{
+//         item.classList.add('done');
+//     });
+//     item.addEventListener('dblclick', ()=>{
+//         toDoList.removeChild(item);
+//     })
+// }
+// }
