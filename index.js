51b1f7dc711b172adef3f7880bf9fd4b35c73d15
lenizza1331 <leniyuva@gmail.com>
2021-11-15 03:17:27 +0300

    2

--- index.js
+++ index.js
@@ -3,7 +3,22 @@ const btn = document.querySelector('.btn');
 const toDoList = document.querySelector('.toDoList');
 
 
-btn.addEventListener('click', enter);
+btn.addEventListener('click', () => {
+    if (inputField.value.length >0){
+    const item = document.createElement('li');
+    item.innerText = inputField.value;
+    item.classList.add('added');
+    toDoList.appendChild(item);
+    inputField.value = '';
+
+    item.addEventListener('click', ()=>{
+        item.classList.add('done');
+    });
+    item.addEventListener('dblclick', ()=>{
+        toDoList.removeChild(item);
+    })
+}
+});
 
 inputField.addEventListener("keypress", enter);
 
@@ -25,20 +40,3 @@ function enter(e) {
 
 
 
-
-// () => {
-//     if (inputField.value.length >0){
-//     const item = document.createElement('li');
-//     item.innerText = inputField.value;
-//     item.classList.add('added');
-//     toDoList.appendChild(item);
-//     inputField.value = '';
-
-//     item.addEventListener('click', ()=>{
-//         item.classList.add('done');
-//     });
-//     item.addEventListener('dblclick', ()=>{
-//         toDoList.removeChild(item);
-//     })
-// }
-// }
